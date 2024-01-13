ccce1ad924c5d91585b2709df3072e4d79f6eb30
Hudson Jones <hudsonjones.china@gmail.com>
2024-01-13 21:07:36 +0800

    so many bugs

--- routers/tasks.py
+++ routers/tasks.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Path
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
+
 from models import Tasks
 from database import get_db
 from .auth import get_current_user
@@ -31,8 +32,8 @@ class Task(BaseModel):
 
 @router.get("", status_code=status.HTTP_200_OK)
 async def get_all_tasks(db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
-    return db.query(Tasks).filter(Tasks.id == current_user.get("id")).all()
-
+    # return db.query(Tasks).filter(Tasks.id == current_user.get("id")).all()
+    return db.query(Tasks).filter(Tasks.author == current_user.get("id")).all()
 
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create_task(task_data: Task, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
@@ -52,16 +53,24 @@ async def get_task_by_id(task_id: int = Path(gt=0), db: Session = Depends(get_db
 
 @router.put("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_task_by_id(task_data: Task, task_id: int = Path(gt=0), db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
-    task = db.query(Tasks).filter(Tasks.id == task_id).filter(Tasks.author == current_user.get("id"))
+
+    print("in the method")
+
+    task = db.query(Tasks).filter(Tasks.id == task_id).filter(Tasks.author == current_user.get("id")).first()
+
+    print(task)
 
     if task is None:
         raise HTTPException(status_code=404, detail=f"Task with id #{task_id} was not found")
 
+    print(task_data)
+
     task.title = task_data.title
-    task.author = task_data.author
     task.description = task_data.description
     task.priority = task_data.priority
     task.complete = task_data.complete
+
+    # print(task)
 
     db.add(task)
     db.commit()
